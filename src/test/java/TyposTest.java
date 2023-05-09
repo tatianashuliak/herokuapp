5b3b9e70719a2395cfbe33ddc2516a0e355fe9f9
Tatiana Shuliak <tatjananikolaevich23041997@gmail.com>
2023-05-09 12:29:14 +0300

    changes added

--- src/test/java/TyposTest.java
+++ src/test/java/TyposTest.java
@@ -4,10 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import static Constants.PageUrls.*;
 public class TyposTest extends BaseTest {
+    public static final String CORRECT_TEXT1 = "This example demonstrates a typo being introduced. It does it randomly on each page load.";
+    public static final String CORRECT_TEXT2 = "Sometimes you'll see a typo, other times you won't.";
+
     @Test
-    public void typosTest() {
+    public void typosTest1() {
         driver.get(TYPOS_PAGE);
-        WebElement text = driver.findElement(By.xpath("//p[contains(text(), 'see')]"));
-        Assert.assertTrue(text.getText().contains("won't"),"Text has typo");
+        WebElement text = driver.findElement(By.xpath("//p[1]"));
+        Assert.assertTrue(text.getText().contains(CORRECT_TEXT1),"Text has typo");
+    }
+
+    @Test
+    public void typosTest2() {
+        driver.get(TYPOS_PAGE);
+        WebElement text = driver.findElement(By.xpath("//p[2]"));
+        Assert.assertTrue(text.getText().contains(CORRECT_TEXT2),"Text has typo");
     }
 }
